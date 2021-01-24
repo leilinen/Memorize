7f009e61191e9b9bc3960041d94e9058bf0058fa
leiline <lamlee1993@outlook.com>
2021-01-24 16:44:15 +0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by 李琳 on 2021/1/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
